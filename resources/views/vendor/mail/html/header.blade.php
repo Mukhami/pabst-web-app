66ee641c2bba9c82c490d9662278ba4d952cce15
Kevin Mukhami <kevin.mukhami@co-foundry.co.za>
2024-05-17 13:24:54 +0300

    Image on email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,17 +1,5 @@
-{{--@props(['url'])--}}
-{{--<tr>--}}
-{{--<td class="header">--}}
-{{--<a href="{{ $url }}" style="display: inline-block;">--}}
-{{--@if (trim($slot) === 'Laravel')--}}
-{{--<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">--}}
-{{--@else--}}
-{{--{{ $slot }}--}}
-{{--@endif--}}
-{{--</a>--}}
-{{--</td>--}}
-{{--</tr>--}}
 <tr>
     <td class="header">
-        <img src="https://www.pabstpatent.com/assets/ui/logo-white.svg" height="100px" alt="Pabst | Mkenga">
+        <img src="https://ppg.alamaworks.com/img/ppg-logo-white.svg" height="100px" alt="Pabst | Mkenga">
     </td>
 </tr>
